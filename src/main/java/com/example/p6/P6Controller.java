79834435211370779bdb1fa6d83534a8d46e183f
Brennan Harris <brennan.harris@inl.gov>
2022-11-23 11:22:15 -0700

    removing sqlite db from version control

--- src/main/java/com/example/p6/P6Controller.java
+++ src/main/java/com/example/p6/P6Controller.java
@@ -3,6 +3,7 @@ package com.example.p6;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import java.util.HashMap;
 
