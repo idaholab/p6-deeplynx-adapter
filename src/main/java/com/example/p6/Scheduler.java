263525796babdcf1f1851f15e5328d6b49aabf7d
Brennan Harris <brennan.harris@inl.gov>
2022-12-01 14:33:29 -0700

    rename Scheduler, actually use env var for interval

--- src/main/java/com/example/p6/Scheduler.java
+++ src/main/java/com/example/p6/Scheduler.java
@@ -4,13 +4,13 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
-public class TESTScheduler {
+public class Scheduler {
 
 	// private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
 
 	// private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
-	@Scheduled(fixedRate = 5000)
+	@Scheduled(fixedRate = parseInt(System.getenv("P6_EXECUTION_INTERVAL")))
 	public void reportCurrentTime() {
 		System.out.println("HIT");
 	}
